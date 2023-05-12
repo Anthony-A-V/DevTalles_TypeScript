8a378adc67699fa7b14786a31ce9d93f8a36377a
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-05-12 08:00:42 -0500

    ok

---
+++ bases/tipos/string.ts
@@ -0,0 +1,13 @@
+(() => {
+
+  const batman: string = 'Batman';
+  const lintenaVerde: string = "Linterna's Verde";
+  const volcanNegro: string = `Héroe: Volcán Negro`;
+
+  const abc = 123;
+
+  console.log(` I'm ${batman} `)
+
+  console.log(batman.toUpperCase())
+
+})()
