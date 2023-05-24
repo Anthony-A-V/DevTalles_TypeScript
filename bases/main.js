8b2302f86a161283ac12ba49929d382d5e35ace7
Anthony-A-V <anthony.anguizvargas@gmail.com>
2023-05-23 22:34:39 -0500

    Cambios

--- bases/main.js
+++ bases/main.js
@@ -1,52 +1,44 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
-    return t;
-};
 (() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        ironman: 'Robert Downey Jr.',
-        vision: 'Paul Bettany',
-        activo: true,
-        poder: 1500.123123
+    ;
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: [1, 2],
     };
-    const printAvenger = (_a) => {
-        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
-        console.log(ironman, resto);
+    let superman = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [1],
+        getName() {
+            return this.name;
+        },
     };
-    const avengersArr = ['Cap. América', true, 150.15];
-    const [capitan, ironman, seriaUnNumero] = avengersArr;
 })();
 (() => {
-    const ironman = {
-        name: 'Ironman',
-        weapon: 'Armorsuit'
+    const client = {
+        name: 'Fernando',
+        age: 25,
+        address: {
+            id: 125,
+            zip: 'KT2 SUD',
+            city: 'Ottawa',
+        },
+        getFullAddress(id) {
+            return this.address.city;
+        },
     };
-    const captainAmerica = {
-        name: 'Capitán América',
-        weapon: 'Escudo'
-    };
-    const thor = {
-        name: 'Thor',
-        weapon: 'Mjolnir'
-    };
-    const avengers = [ironman, thor, captainAmerica];
-    for (let avenger of avengers) {
-        console.log(avenger.name, avenger.weapon);
-    }
-})();
-(() => {
-    const nombre = 'Fernando';
-    const getName = () => {
-        console.log('viejo getName');
+    const client2 = {
+        name: 'Melissa',
+        age: 30,
+        address: {
+            city: 'Toronto',
+            id: 120,
+            zip: 'K2S USA',
+        },
+        getFullAddress(id) {
+            return this.address.city;
+        },
     };
 })();
 //# sourceMappingURL=main.js.map
